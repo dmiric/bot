2f9ef9f91978822dd995afb7cb23d35aa2073b1d
dmiric <damir.miric@gmail.com>
2021-02-19 12:52:55 +0100

    fix port

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
   const argvService = app.select(InputModule).get(ArgvService, { strict: true })
   const port = argvService.getPort()
   
-  await app.listen(port);
+  await app.listen(8080);
 }
 bootstrap();
